e45b303cab774bc1ffe36b74aed12667d5f1d80a
johnny-sack1 <jonymaka@gmail.com>
2018-10-09 05:17:42 +0200

    Implement Node class

--- src/main/java/com/codecool/queue/Node.java
+++ src/main/java/com/codecool/queue/Node.java
@@ -1,4 +1,22 @@
 package com.codecool.queue;
 
 public class Node {
+    private String value;
+    private Node next;
+
+    public Node(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
