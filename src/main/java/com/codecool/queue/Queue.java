4eea94433abc9a7fe64f0f0db3b25df57c5c8b6a
johnny-sack1 <jonymaka@gmail.com>
2018-10-09 05:56:09 +0200

    Implement enqueue()

--- src/main/java/com/codecool/queue/Queue.java
+++ src/main/java/com/codecool/queue/Queue.java
@@ -1,4 +1,58 @@
 package com.codecool.queue;
 
 public class Queue {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void enqueue(String value) {
+        Node newNode = new Node(value);
+
+        if (isEmpty()) {
+            head = newNode;
+            tail = head;
+        }
+        else {
+            Node current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+            tail = current.getNext();
+        }
+        size++;
+    }
+
+
+    public String peek() {
+        return "";
+    }
+
+    public String dequeue() {
+        return "";
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        Node current;
+        if (!isEmpty()) {
+            builder.append(" " + head.getValue());
+            current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+                builder.append(" " + current.getValue());
+            }
+        }
+        return builder.toString();
+    }
 }
--- src/test/java/com/codecool/queue/QueueTest.java
+++ src/test/java/com/codecool/queue/QueueTest.java
@@ -1,4 +1,24 @@
 package com.codecool.queue;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class QueueTest {
+    private Queue queue;
+
+    @BeforeEach
+    void setup() {
+        queue = new Queue();
+    }
+
+    @Test
+    void testEnqueue() {
+        queue.enqueue("first");
+        queue.enqueue("second");
+        queue.enqueue("third");
+        String result = " first second third";
+        assertEquals(result, queue.toString());
+    }
 }
